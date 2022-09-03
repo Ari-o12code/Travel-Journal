040c0193f735722638d30f0b535922f58b95ae11
Quadri Alameen <quadrameen@gmail.com>
2022-09-03 22:21:13 +0100

    Updates

---
+++ precache-manifest.b6ebbe76dada3939927d79108d364c66.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e4b036fd3a49b3545dd",
+    "url": "/Travel-Journal/static/css/main.35981a9b.chunk.css"
+  },
+  {
+    "revision": "7e4b036fd3a49b3545dd",
+    "url": "/Travel-Journal/static/js/main.7e4b036f.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/Travel-Journal/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "b32f1f818a97b5fe47e8",
+    "url": "/Travel-Journal/static/css/2.6944f2b1.chunk.css"
+  },
+  {
+    "revision": "b32f1f818a97b5fe47e8",
+    "url": "/Travel-Journal/static/js/2.b32f1f81.chunk.js"
+  },
+  {
+    "revision": "eb918f8f55dae855823e",
+    "url": "/Travel-Journal/static/js/runtime~main.eb918f8f.js"
+  },
+  {
+    "revision": "ae36ce15c754eb80dda5df2cee1f1518",
+    "url": "/Travel-Journal/static/media/Rectangle 77.ae36ce15.png"
+  },
+  {
+    "revision": "f00101c589c56f36617df4d3b2f353e3",
+    "url": "/Travel-Journal/static/media/Rectangle 84.f00101c5.png"
+  },
+  {
+    "revision": "efac4d87a9c166a15f5315a117b1a64b",
+    "url": "/Travel-Journal/static/media/Rectangle 78.efac4d87.png"
+  },
+  {
+    "revision": "c37e78550a9c737d4652c25ec1938b1f",
+    "url": "/Travel-Journal/index.html"
+  }
+];
